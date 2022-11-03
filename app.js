2358ef131e7cb5c0ea402646b751381957532086
GraemeAngusJones <86078388+GraemeAngusJones@users.noreply.github.com>
2022-11-03 17:03:37 +0000

    Refactor complete

--- app.js
+++ app.js
@@ -17,20 +17,52 @@ app.use(cors({ origin: '*' }));
 
 // Controllers -----------------------------------
 
-const buildModulesSelectSql = (whereField, id, isUsersExtended) => {
+const buildModulesSelectSql = (id, variant) => {
   let table = '((Modules LEFT JOIN Users ON ModuleLeaderID=UserID) LEFT JOIN Years ON ModuleYearID=YearID )';
   let fields = ['ModuleID', 'ModuleName', 'ModuleCode', 'ModuleLevel', 'ModuleYearID', 'ModuleLeaderID', 'ModuleImageURL', 'CONCAT(UserFirstname," ",UserLastname) AS ModuleLeaderName', 'YearName AS ModuleYearName'];
-  if (isUsersExtended) {
-    table = `Modulemembers INNER JOIN ${table} ON Modulemembers.ModulememberModuleID=Modules.ModuleID`;
+  let sql = '';
+
+  switch (variant) {
+    case 'leader':
+      sql = `SELECT ${fields} FROM ${table} WHERE ModuleLeaderID=${id}`;
+      break;
+    case 'users':
+      table = `Modulemembers INNER JOIN ${table} ON Modulemembers.ModulememberModuleID=Modules.ModuleID`;
+      sql = `SELECT ${fields} FROM ${table} WHERE ModulememberUserID=${id}`;
+      break;
+    default:
+      sql = `SELECT ${fields} FROM ${table}`;
+      if (id) sql += ` WHERE ModuleID=${id}`;
   }
-  let sql = `SELECT ${fields} FROM ${table}`;
-  if (id) sql += ` WHERE ${whereField}=${id}`;
+
+  return sql;
+};
+
+const buildUsersSelectSql = (id, variant) => {
+  let table = '((Users LEFT JOIN Usertypes ON UserUsertypeID=UsertypeID) LEFT JOIN Years ON UserYearID=YearID )';
+  let fields = ['UserID', 'UserFirstname', 'UserLastname', 'UserEmail', 'UserLevel', 'UserYearID', 'UserUsertypeID', 'UserImageURL', 'UsertypeName AS UserUsertypeName', 'YearName AS UserYearName'];
+  let sql = '';
+
+  switch (variant) {
+    case 'student':
+      sql = `SELECT ${fields} FROM ${table} WHERE UserUsertypeID=2`;
+      break;
+    case 'staff':
+      sql = `SELECT ${fields} FROM ${table} WHERE UserUsertypeID=1`;
+      break;
+    case 'groups':
+      table = `Groupmembers INNER JOIN ${table} ON Groupmembers.GroupmemberUserID=Users.UserID`;
+      sql = `SELECT ${fields} FROM ${table} WHERE GroupmemberGroupID=${id}`;
+      break;
+    default:
+      sql = `SELECT ${fields} FROM ${table}`;
+      if (id) sql += ` WHERE UserID=${id}`;
+  }
   
   return sql;
 };
 
-const readModules = async (whereField, id, isUsersExtended) => {
-  const sql = buildModulesSelectSql(whereField, id, isUsersExtended);
+const read = async (sql) => {
   try {
     const [result] = await database.query(sql);
     return (result.length === 0)
@@ -42,47 +74,24 @@ const readModules = async (whereField, id, isUsersExtended) => {
   }
 };
 
-const modulesController = async (res, whereField, id, isUsersExtended) => {
+const getModulesController = async (res, id, variant) => {
   // Validate request
 
   // Access data
-  const { isSuccess, result, message: accessorMessage } = await readModules(whereField, id, isUsersExtended);
+  const sql = buildModulesSelectSql(id, variant);
+  const { isSuccess, result, message: accessorMessage } = await read(sql);
   if (!isSuccess) return res.status(400).json({ message: accessorMessage });
   
   // Response to request
   res.status(200).json(result);
 };
 
-const buildUsersSelectSql = (whereField, id, isGroupsExtended) => {
-  let table = '((Users LEFT JOIN Usertypes ON UserUsertypeID=UsertypeID) LEFT JOIN Years ON UserYearID=YearID )';
-  let fields = ['UserID', 'UserFirstname', 'UserLastname', 'UserEmail', 'UserLevel', 'UserYearID', 'UserUsertypeID', 'UserImageURL', 'UsertypeName AS UserUsertypeName', 'YearName AS UserYearName'];
-  if (isGroupsExtended) {
-    table = `Groupmembers INNER JOIN ${table} ON Groupmembers.GroupmemberUserID=Users.UserID`;
-  }
-  let sql = `SELECT ${fields} FROM ${table}`;
-  if (id) sql += ` WHERE ${whereField}=${id}`;
-
-  return sql;
-};
-
-const readUsers = async (whereField, id, isGroupsExtended) => {
-  const sql = buildUsersSelectSql(whereField, id, isGroupsExtended);
-  try {
-    const [result] = await database.query(sql);
-    return (result.length === 0)
-      ? { isSuccess: false, result: null, message: 'No record(s) found' }
-      : { isSuccess: true, result: result, message: 'Record(s) successfully recovered' };
-  }
-  catch (error) {
-    return { isSuccess: false, result: null, message: `Failed to execute query: ${error.message}` };
-  }
-};
-
-const usersController = async (res, whereField, id, isGroupsExtended) => {
+const getUsersController = async (res, id, variant) => {
   // Validate request
 
   // Access data
-  const { isSuccess, result, message: accessorMessage } = await readUsers(whereField, id, isGroupsExtended);
+  const sql = buildUsersSelectSql(id, variant);
+  const { isSuccess, result, message: accessorMessage } = await read(sql);
   if (!isSuccess) return res.status(400).json({ message: accessorMessage });
   
   // Response to request
@@ -91,19 +100,17 @@ const usersController = async (res, whereField, id, isGroupsExtended) => {
 
 // Endpoints -------------------------------------
 // Modules
-app.get('/api/modules', (req, res) => modulesController(res, null, null, false));
-app.get('/api/modules/:id(\d+)', (req, res) => modulesController(res, "ModuleID", req.params.id, false));
-app.get('/api/modules/leader/:id', (req, res) => modulesController(res, "ModuleLeaderID", req.params.id, false));
-app.get('/api/modules/users/:id', (req, res) => modulesController(res, "ModulememberUserID", req.params.id, true));
+app.get('/api/modules', (req, res) =>            getModulesController(res, null,          null));
+app.get('/api/modules/:id(\\d+)', (req, res) =>  getModulesController(res, req.params.id, null));
+app.get('/api/modules/leader/:id', (req, res) => getModulesController(res, req.params.id, 'leader'));
+app.get('/api/modules/users/:id', (req, res) =>  getModulesController(res, req.params.id, 'users' ));
 
 // Users
-const STAFF = 1;
-const STUDENT = 2;
-app.get('/api/users', (req, res) => usersController(res, null, null, false));
-app.get('/api/users/:id(\d+)', (req, res) => usersController(res, "UserID", req.params.id, false));
-app.get('/api/users/student', (req, res) => usersController(res, "UsertypeID", STUDENT, false));
-app.get('/api/users/staff', (req, res) => usersController(res, "UsertypeID", STAFF, false));
-app.get('/api/users/groups/:id', (req, res) => usersController(res, "GroupmemberGroupID", req.params.id, true));
+app.get('/api/users', (req, res) =>            getUsersController(res, null,          null));
+app.get('/api/users/:id(\\d+)', (req, res) =>  getUsersController(res, req.params.id, null));
+app.get('/api/users/student', (req, res) =>    getUsersController(res, null,          'student'));
+app.get('/api/users/staff', (req, res) =>      getUsersController(res, null,          'staff'));
+app.get('/api/users/groups/:id', (req, res) => getUsersController(res, req.params.id, 'groups'));
 
 // Start server ----------------------------------
 const PORT = process.env.PORT || 5000;
